3d3d056749dc6215a9a3b0c44e008024b0a4983d
faheem <lalafahim55@gmail.com>
2021-12-07 19:25:42 +0530

    Faheem UC13 for creating employee obj

--- js/Employeeform.js
+++ js/Employeeform.js
@@ -11,7 +11,6 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     //name validation
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
-    output.textContent=name.value;
     name.addEventListener('input', function() {
         if (name.value.length == 0) {
             textError.textContent = "";
@@ -25,4 +24,47 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         }
     });
 });
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    } catch (e) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayrollData.start_date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
 
